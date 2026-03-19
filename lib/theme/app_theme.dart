eda6064fcf40f3b8594a7bad0380b1c65f2b1bf9
lindtseyxvii <giousalvador@gmail.com>
2026-03-20 02:09:13 +0800

    feat: add navigationBarTheme and default fontFamily for lightTheme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,6 +3,8 @@ import '../constants/app_colors.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Inter',
     brightness: Brightness.light,
     colorScheme: const ColorScheme.light(
       primary: Color(0xFF304166),
@@ -13,6 +15,33 @@ class AppTheme {
         backgroundColor: AppColors.lightButtonBackground,
         foregroundColor: AppColors.lightButtonForeground,
       ),
+    ),
+    navigationBarTheme: NavigationBarThemeData(
+      backgroundColor: const Color(0xff304166),
+      indicatorColor: const Color(0xff2D68FF),
+
+      // --- ICON COLORS ---
+      iconTheme: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.selected)) {
+          return const IconThemeData(color: Colors.white);
+        }
+        return IconThemeData(color: Colors.white.withValues(alpha: 0.5));
+      }),
+
+      // --- LABEL COLORS ---
+      labelTextStyle: WidgetStateProperty.resolveWith((states) {
+        if (states.contains(WidgetState.selected)) {
+          return const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          );
+        }
+        return TextStyle(
+          color: Colors.white.withValues(alpha: 0.5),
+          fontSize: 12,
+        );
+      }),
     ),
   );
 }
