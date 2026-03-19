47540112ddcd2990be896b3ac82169f944de3f5a
lindtseyxvii <giousalvador@gmail.com>
2026-03-20 02:10:06 +0800

    feat: add new routes and paths: signin, personalization, main, dashboard

--- lib/constants/route_names.dart
+++ lib/constants/route_names.dart
@@ -2,4 +2,8 @@
 class RouteNames {
   static const String login = '/login';
   static const String signup = '/signup';
+  static const String signin = '/signin';
+  static const String personalization = '/personalization';
+  static const String main = '/main';
+  static const String dashboard = '/dashboard';
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:signmirror_flutter/screens/dashboard_screen.dart';
+import 'package:signmirror_flutter/screens/main_screen.dart';
+import 'package:signmirror_flutter/screens/personalization_screen.dart';
 // Import your screens here
 import '../screens/screens.dart';
 import "../constants/route_names.dart";
@@ -7,12 +10,20 @@ class AppRoutes {
   // Define route name constants
   static const String login = '/login';
   static const String signup = '/signup';
+  static const String signin = '/signin';
+  static const String personalization = '/personalization';
+  static const String dashboard = '/dashboard';
+  static const String main = '/main';
 
   // Create the route map
   static Map<String, WidgetBuilder> getRoutes() {
     return {
-      RouteNames.login: (context) => const LoginScreen(),
+      RouteNames.login: (context) => const OnboardingScreen(),
       RouteNames.signup: (context) => const SignupScreen(),
+      RouteNames.signin: (context) => const SigninScreen(),
+      RouteNames.personalization: (context) => const PersonalizationScreen(),
+      RouteNames.main: (context) => const MainScreen(),
+      RouteNames.dashboard: (context) => const DashboardScreen(),
     };
   }
 }
