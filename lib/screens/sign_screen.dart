59556d2a2305bb8dee47bfb1798ce5aafc5ce5bd
lindtseyxvii <giousalvador@gmail.com>
2026-03-21 13:19:36 +0800

    feat: add SignScreen widget for displaying sign-related content

---
+++ lib/screens/sign_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignScreen extends StatefulWidget {
+  const SignScreen({super.key});
+
+  @override
+  State<SignScreen> createState() => _SignScreenState();
+}
+
+class _SignScreenState extends State<SignScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
